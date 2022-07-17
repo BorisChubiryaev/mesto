cda59164a22bf367071d9215df65eb32242707d1
BorisChubiryaev <boriks7@yandex.ru>
2022-07-18 01:19:46 +0300

    2V Project 5 1edits
    first try

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 # Обзор
 
  Интерактивная страница, имеющая на данный момент просматривать фотографии и изменять профиль.
+ Во второй версии проекта была внедрена возможность добавлять, удалять карточки, а так же ставить им лайки.
 
 # Стек 
 
@@ -19,8 +20,9 @@
 
 # Макет
 
- * Ссылка на макет в Figma - https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1
+ * Ссылка на макет первой версии в Figma - https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1
+ * Ссылка на макет второй версии в Figma - https://www.figma.com/file/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=50160%3A172
 
-# ССылка на проект
+# Ссылка на проект
 
  https://borischubiryaev.github.io/mesto/index.html
--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,130 @@
-const popup = document.querySelector('.popup');
-const openPopup = document.querySelector('.profile__edit-button');
-const closePopup = popup.querySelector('.popup__close');
+const popupEdit = document.querySelector('.popup__container');
+const nameInput = document.querySelector('input[name="name"]');
+const aboutInput = document.querySelector('input[name="about"]');
+const usName = document.querySelector('.profile__name');
+const description = document.querySelector('.profile__description');
+const formEdit = document.querySelector('.popup__form_profile');
+const editButton = document.querySelector('.profile__edit-button');
+const closePopupEdit = document.querySelector('.popup__close-edit');
 
-const popupSave = popup.querySelector('.popup__form');
-let usName = document.querySelector('.profile__name');
-let description = document.querySelector('.profile__description');
-let formName = document.querySelector('.popup__input-name');
-let formDescription = document.querySelector('.popup__input-about');
+const popupAdd = document.querySelector('.popup__add');
+const formAdd = document.querySelector('.popup__form-card');
+const inputPlace = document.querySelector('.popup__form-input_card_name');
+const inputUrl = document.querySelector('.popup__form-input_card_url');
+const closePopupAdd = document.querySelector('#popup__close_card');
+const addButton = document.querySelector('.profile__add-button');
 
+const photo = document.querySelector('.popup_picture');
+const image = document.querySelector('.popup__image');
+const closeImage = document.querySelector('.popup__close-image');
+const descriptionImage = document.querySelector('.popup__image-description');
 
-const onPopup = function() {
-    popup.classList.toggle('popup_opened');
-};
+const elements = document.querySelector('.elements');
 
-const offPopup = function() {
-    popup.classList.remove('popup_opened');
+const initialCards = [
+  {
+    name: 'Миша',
+    link: 'https://sun9-east.userapi.com/sun9-18/s/v1/if2/y8NHb0zVNi-TU_WGU0R8eF3JqQZpn4LcE2qUESbcPdGpP6oCIyRlUtvftWqHCsGt8_KTNlHAtt3_7a5alF7Wr283.jpg?size=322x574&quality=96&type=album'
+  },
+  {
+    name: 'School',
+    link: 'https://sun9-north.userapi.com/sun9-88/s/v1/if1/43c8AIOEy7ivwnIMU1fMDZD1sMwfFiM5uyeeMmtLNC32AuMlx53N03xuQ3yCFB61cvfmjVu5.jpg?size=650x599&quality=96&type=album'
+  },
+  {
+    name: 'Котенок',
+    link: 'https://sun9-north.userapi.com/sun9-88/s/v1/if1/VLqmvuWgyiSxdg_m5aRel0IlMzQakOZx6jrT7sVLzF24ZyBTE6pT6s5A-FRkg-UyuxpP-ctI.jpg?size=604x579&quality=96&type=album'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
-openPopup.addEventListener('click', onPopup);
-closePopup.addEventListener('click', offPopup);
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
 
-function formSumbit(evt) {
+function editPopupProfile() {
+  nameInput.value = usName.textContent;
+  aboutInput.value = description.textContent;
+  openPopup(popupEdit);
+}
+
+function editPopupProfileSave(evt) {
+  evt.preventDefault();
+  usName.textContent = nameInput.value;
+  description.textContent = aboutInput.value;
+  closePopup(popupEdit);
+}
+
+function deleteCard(evt) {
+  evt.target.closest('.element').remove();
+}
+
+function likeCard(evt) {
+  evt.target.classList.toggle('element__like_active');
+}
+
+function createCard(item) {
+    const templateElement = document.querySelector('.template-element').content;
+    const newElement = templateElement.querySelector('.element').cloneNode(true);
+    const elementPhoto = newElement.querySelector('.element__photo');
+    const elementTitle = newElement.querySelector('.element__title');
+    const elementLike = newElement.querySelector('.element__like');
+    const elementDelete = newElement.querySelector('.element__delete');
+
+    elementTitle.textContent = item.name;
+    elementPhoto.alt = item.name;
+    elementPhoto.src = item.link;
+
+    elementDelete.addEventListener('click', deleteCard);
+    elementLike.addEventListener('click', likeCard);
+    elementPhoto.addEventListener('click', () => openImage(item.link, item.name));
+
+    return newElement;
+}
+
+function createList() {
+    const baseCards = initialCards.map(item => {
+        const newElement = createCard(item);
+        return newElement;
+    });
+    elements.append(...baseCards);
+}
+
+function addCards(evt) {
     evt.preventDefault();
-    usName.textContent = formName.value;
-    description.textContent = formDescription.value;
-    onPopup();
+    const card = createCard({name: inputPlace.value, link: inputUrl.value});
+    elements.prepend(card);
+    closePopup(popupAdd);
+    formAdd.reset();
 }
 
-popupSave.addEventListener('submit', formSumbit);
+function openImage(link, alt) {
+    image.src = link;
+    image.alt = alt;
+    descriptionImage.textContent = alt;
+    openPopup(photo);
+}
+
+createList();
+
+addButton.addEventListener('click', () => openPopup(popupAdd));
+closePopupAdd.addEventListener('click', () => closePopup(popupAdd));
+editButton.addEventListener('click', editPopupProfile);
+formEdit.addEventListener('submit', editPopupProfileSave);
+closePopupEdit.addEventListener('click', () => closePopup(popupEdit));
+formAdd.addEventListener('submit', addCards);
+closeImage.addEventListener('click', () => closePopup(photo));
